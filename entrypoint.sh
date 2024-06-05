c5977c0141d9264617c7f9d50c7e226959219509
Arif Hanif <ahanif@aeieng.com>
2024-06-05 17:52:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ REGISTRATION_TOKEN="$(curl -X POST -fsSL \
   "$REGISTRATION_TOKEN_API_URL" \
   | jq -r '.token')"
 
-./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --unattended --ephemeral && ./run.sh
+./config.sh --url $GH_URL --token $REGISTRATION_TOKEN --labels $LABELS --unattended --ephemeral && ./run.sh
